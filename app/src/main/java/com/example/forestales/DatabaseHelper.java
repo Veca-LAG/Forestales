3ab4b103a6f784d5d41bf5829ba1ff7df25b7896
Veca-LAG <veca731205@gmail.com>
2025-05-04 13:49:08 -0600

    orden

---
+++ app/src/main/java/com/example/forestales/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.example.forestales;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+public class DatabaseHelper {
+    public static final String databaseName = "Forestales.db";
+
+}
